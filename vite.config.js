e06dc06263a18f8bc5fde2c8637b380feb100afb
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2022-10-31 05:59:30 +0100

    Add Watch mode for css but still bug for export file

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,15 @@ import { resolve } from "path"
 
 export default defineConfig({
     root: resolve("sources"),
-    vite: {
-        css:
-        {
-            preprocessorOptions: {
-                scss: {
-                    additionalData: `@import "src/styles/main.scss";`
-                }
-            }
-        }
-    },
     server: {
         port: 8000,
         host: "0.0.0.0"
+    },
+
+    scss: {
+        watch: "sources/src/scss/main.scss",
+        output: function(styles, styleNodes) {
+            console.log(styles)
+        }
     }
 })
