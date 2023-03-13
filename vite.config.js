7c4d05c6bc50f79dcfd36337605826d59feae203
Pietrucci-Blacher <maxime.pietrucci@gmail.com>
2023-03-13 19:22:18 +0100

    Add Vitest, prettier and new stylelint conf

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,12 @@
 import { defineConfig } from "vite"
 import { resolve } from "path"
 
-const assetsDir = "assets/"
-const outputDefaults = {
-    entryFileNames: `${assetsDir}[name].js`,
-    chunkFileNames: `${assetsDir}[name].js`,
-    assetFileNames: `${assetsDir}[name].[ext]`,
-}
-
 export default defineConfig({
     root: resolve("sources"),
     base: "./",
     server: {
         port: 8000,
         host: "0.0.0.0",
-    },
-    build: {
-        manifest: true,
-        rollupOptions: {
-            output: {
-                ...outputDefaults,
-            }
-        }
     },
     preview: {
         port: 8001,
